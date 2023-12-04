91205e41f9eb9445d12c46c64334acdae0d499a1
mariocesar33 <mcgato33@hotmail.com>
2023-12-04 09:50:08 -0100

    Abstraindo criação de entidades

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 import { Answer } from "../entities/answer"
 import { AnswerRepository } from "../repositories/answers-repository"
 
@@ -12,10 +13,10 @@ export class AnswerQuestionUseCase {
   constructor( private answersRepository: AnswerRepository) {}
 
   async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({
+    const answer = Answer.create({
       content,
-      authorId: instructorId,
-      questionId,
+      authorId: new UniqueEntityId(instructorId),
+      questionId: new UniqueEntityId(questionId),
     })
 
     await this.answersRepository.create(answer)
