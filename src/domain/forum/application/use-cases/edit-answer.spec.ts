e80819d84fc4d7a3159a7c50bcf1bef24d2d2310
mariocesar33 <mcgato33@hotmail.com>
2023-12-19 15:46:30 -0100

    Caso de uso: Editar resposta

---
+++ src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -0,0 +1,59 @@
+import { EditAnswerUseCase } from './edit-answer'
+import { InMemoryAnswersRepository } from '@/test/repositories/in-memory-answers-repository'
+
+import { makeAnswer } from '@/test/factories/make-answer'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: EditAnswerUseCase
+
+describe('Edit Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new EditAnswerUseCase(inMemoryAnswersRepository)
+  })
+
+  it('should be able to edit a answer', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityId('author-1'),
+      },
+      new UniqueEntityId('answer-1'),
+    )
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await sut.execute({
+      authorId: 'author-1',
+      answerId: newAnswer.id.toString(),
+      content: 'Conteúdo teste',
+    })
+
+    // toMatchObject => Vai verificar se as propriedades que estou passsado,
+    // estão dentro do item (item[0]) que estou passando também.
+    expect(inMemoryAnswersRepository.items[0]).toMatchObject({
+      content: 'Conteúdo teste',
+    })
+  })
+
+  it('should not be able to edit a answer from another user', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityId('author-1'),
+      },
+      new UniqueEntityId('answer-1'),
+    )
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    // Espero que quando chamo o caso de uso de editar de uma pergunta,
+    // passando o ID do autor que não é o autor da pergunta ele deve rejeitar, e retorna um erro.
+    expect(() => {
+      return sut.execute({
+        authorId: 'author-2',
+        answerId: newAnswer.id.toString(),
+        content: 'Conteúdo teste',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
