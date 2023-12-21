aa9bc7dd85a07562c1ac2f307f7ed867b87355e1
mariocesar33 <mcgato33@hotmail.com>
2023-12-21 13:17:55 -0100

    Entidades de comentários

---
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -0,0 +1,54 @@
+import { Entity } from '@/core/entities/entity'
+import { Optional } from '@/core/types/optional'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+export interface AnswerCommentProps {
+  authorId: UniqueEntityId
+  answerId: UniqueEntityId
+  content: string
+  createdAt: Date
+  updatedAt?: Date
+}
+
+// Comentário de uma resposta
+export class AnswerComment extends Entity<AnswerCommentProps> {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  static create(
+    props: Optional<AnswerCommentProps, 'createdAt'>,
+    id?: UniqueEntityId,
+  ) {
+    const answerComment = new AnswerComment(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return answerComment
+  }
+}
