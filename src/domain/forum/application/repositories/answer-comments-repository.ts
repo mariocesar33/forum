676b6bed335e1a33c34473e15b035091accb4292
mariocesar33 <mcgato33@hotmail.com>
2023-12-23 12:50:33 -0100

    Caso de uso: Listar comentários da resposta

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,7 +1,12 @@
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+import { PaginationParams } from '@/core/repositories/pagination-params'
 
 export interface AnswerCommentsRepository {
   create(answerComment: AnswerComment): Promise<void>
   delete(answerComment: AnswerComment): Promise<void>
+  findManyByAnswerId(
+    answerId: string,
+    params: PaginationParams,
+  ): Promise<AnswerComment[]>
   findById(id: string): Promise<AnswerComment | null>
 }
--- src/test/repositories/in-memory-answer-comments-repository.ts
+++ src/test/repositories/in-memory-answer-comments-repository.ts
@@ -1,5 +1,6 @@
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
+import { PaginationParams } from '@/core/repositories/pagination-params'
 
 export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
@@ -16,6 +17,14 @@ export class InMemoryAnswerCommentsRepository
     )
 
     this.items.splice(itemIndex, 1)
+  }
+
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answerComments
   }
 
   async findById(id: string) {
